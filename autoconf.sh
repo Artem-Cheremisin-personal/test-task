9ef0ccd0834db15634b6b0eef4110d7b25bd8c58
Artem-Cheremisin-personal <130326979+Artem-Cheremisin-personal@users.noreply.github.com>
2023-04-10 03:04:39 +0100

    Update autoconf.sh

--- autoconf.sh
+++ autoconf.sh
@@ -12,6 +12,29 @@ while getopts "p:r:k:d:c:f:" arg; do
     f)    force=$OPTARG;;
   esac
 done
+
+function install_docker {
+    sudo apt-get update 
+    sudo apt-get install curl ca-certificates git software-properties-common apt-transport-https
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
+    sudo apt install docker-ce
+    sudo usermod -aG docker ${USER} # add current user to docker gruop , to be able to execute docker commands 
+    sudo systemctl enable docker   
+}
+function install_kubectl {
+    sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo apt-get update
+    sudo apt-get install -y kubectl
+}
+function install_kind {
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/kind    
+}
 if [ "${docker}" == "true" ] || [ "${force}" = "true" ]; then
     install_docker
 fi
@@ -19,7 +42,7 @@ if [ "${kubectl}" == "true" ] || [ "${force}" = "true" ]; then
     install_kubectl
 fi
 if [ "${kind}" == "true" ] || [ "${force}" = "true" ]; then
-    install_kubectl
+    install_kind
 fi
 if [ -z "${password}" ]; then 
     echo "Please specify password for redis" 
@@ -52,25 +75,3 @@ kubectl exec -it redis-0 -n redis -- sh -c \
  "redis-cli --cluster create redis-0.redis.redis.svc.cluster.local:6379 redis-1.redis.redis.svc.cluster.local:6379 redis-2.redis.redis.svc.cluster.local:6379 redis-3.redis.redis.svc.cluster.local:6379 redis-4.redis.redis.svc.cluster.local:6379 redis-5.redis.redis.svc.cluster.local:6379 -a ${password} --cluster-replicas 1 --cluster-yes"
  helm install web-app web-app/ -n web-app
 
-function install_docker() {
-    sudo apt-get update 
-    sudo apt-get install curl ca-certificates git software-properties-common apt-transport-https
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    apt-cache policy docker-ce
-    sudo apt install docker-ce
-    sudo usermod -aG docker ${USER} # add current user to docker gruop , to be able to execute docker commands 
-    sudo systemctl enable docker   
-}
-function install_kubectl() {
-    sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    sudo apt-get update
-    sudo apt-get install -y kubectl
-}
-function install_kind () {
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64
-    chmod +x ./kind
-    sudo mv ./kind /usr/local/bin/kind    
-}
